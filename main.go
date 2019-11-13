0ca27b9f027745ceda8b7d24907bf6bc1e08316d
Nick Frichette <nick@frichetten.com>
2019-11-13 07:35:13 -0600

    Implemented simple cli flags. Implemented the flags for:    encrypt    decrypt    s (single file)

--- main.go
+++ main.go
@@ -1,6 +1,38 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+// CLI flags
+var (
+	encryptFlag    = flag.Bool("encrypt", false, "boolean flag to encrypt")
+	decryptFlag    = flag.Bool("decrypt", false, "boolean flag to decrypt")
+	singleFileName = flag.String("s", "", "encrypt/decrypt a single file. Expects a file name")
+)
+
 func main() {
-	println("Initial Commit")
-	// This is just to give the GitHub Action something to build
+	// Parse cli flags
+	flag.Parse()
+
+	// Validate flags
+	if !validateCLIFlags() {
+		return
+	}
+}
+
+func validateCLIFlags() bool {
+	if *encryptFlag && *decryptFlag {
+		fmt.Println("You can't encrypt and decrypt at the same time")
+		fmt.Println("Terminating")
+		return false
+	}
+	if (*singleFileName != "") && (!*encryptFlag && !*decryptFlag) {
+		fmt.Println("Need to encrypt or decrypt the file")
+		fmt.Println("Please add --encrypt or --decrypt")
+		fmt.Println("Terminating")
+		return false
+	}
+	return true
 }
