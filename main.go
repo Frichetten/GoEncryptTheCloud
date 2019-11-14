1917e6717f4b148a001cea974f8673dc55cdf977
Nick Frichette <nick@frichetten.com>
2019-11-14 09:50:31 -0600

    Added the ability to encrypt a directory. Some bugs to fix:    1. Don't delete the original directory    2. Separate the encrypted and decrypted files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/frichetten/GoEncryptTheCloud/cryptography"
@@ -14,6 +15,7 @@ import (
 var (
 	encryptFlag    = flag.Bool("encrypt", false, "boolean flag to encrypt")
 	decryptFlag    = flag.Bool("decrypt", false, "boolean flag to decrypt")
+	directoryName  = flag.String("dir", "", "Directory of files to encrypt")
 	singleFileName = flag.String("s", "", "Encrypt/decrypt a single file. Expects a file name")
 	outputFileName = flag.String("o", "", "Decrypt a single file and give it this name")
 )
@@ -77,6 +79,41 @@ func main() {
 			fileoperations.WriteFile(*singleFileName, plaintext)
 		}
 
+	} else if *directoryName != "" && *encryptFlag {
+		// Encrypt a directory
+		userPassword := userinput.GetEncryptionKey(*encryptFlag)
+		encryptionKey := cryptography.SHA256Hash(userPassword)
+
+		fileNames := fileoperations.EnumerateDirectory(*directoryName)
+		var directories = []string{}
+		for _, file := range fileNames {
+			fd, err := os.Stat(file)
+			if err != nil {
+				panic(err)
+			}
+			switch mode := fd.Mode(); {
+			case mode.IsDir():
+				directories = append(directories, file)
+			case mode.IsRegular():
+				// Read the file into memory
+				data := fileoperations.ReadFile(file)
+
+				// Encrypt
+				ciphertext, err := cryptography.Encrypt(data, encryptionKey)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				// Write to file
+				fileoperations.WriteFile(file+".enc", ciphertext)
+			}
+		}
+
+		// Now let's modify those directory names
+		for _, directory := range directories {
+			directory = strings.TrimSuffix(directory, "/")
+			os.Rename(directory, directory+".enc")
+		}
 	}
 }
 
@@ -94,6 +131,11 @@ func validateCLIFlags() bool {
 	}
 	if *decryptFlag && *outputFileName != "" && *singleFileName == "" {
 		fmt.Println("If you are changing the output name I need to know what file to decrypt")
+		fmt.Println("Terminating")
+		return false
+	}
+	if *singleFileName != "" && *directoryName != "" {
+		fmt.Println("You can't encrypt/decrypt a directory and a single file at the same time")
 		fmt.Println("Terminating")
 		return false
 	}
